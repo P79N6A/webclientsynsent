d620f8d85edaec63fdcdf2eda5bc4cf958422e4c
Kevin Wen <wenbo0707@gmail.com>
2019-01-29 10:48:22 +0800

    eventloop to 2

--- src/main/java/com/example/Bootstrap.java
+++ src/main/java/com/example/Bootstrap.java
@@ -102,7 +102,7 @@ public final class Bootstrap {
         // -Dvertx.disableDnsResolver=true
         //
         VertxOptions options = new VertxOptions()
-                .setEventLoopPoolSize(1)
+                .setEventLoopPoolSize(2)
                 .setAddressResolverOptions(
                         new AddressResolverOptions()
                                 .setCacheMinTimeToLive(0)
