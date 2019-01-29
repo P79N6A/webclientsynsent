b8db8650dc301cbbd0915c65503a3f14252e078d
Kevin Wen <wenbo0707@gmail.com>
2019-01-29 11:05:22 +0800

    fix macsysctl

--- macossysctl.sh
+++ macossysctl.sh
@@ -3,7 +3,7 @@ sudo sysctl -w net.inet.tcp.win_scale_factor=8
 sudo sysctl -w net.inet.tcp.autorcvbufmax=33554432
 sudo sysctl -w net.inet.tcp.autosndbufinc=16384
 sudo sysctl -w net.inet.tcp.autosndbufmax=33554432
-sudo sysctl -w kern.ipc.maxsockbuf= 4194304
+sudo sysctl -w kern.ipc.maxsockbuf=4194304
 sudo sysctl -w net.inet.tcp.sendspace=1042560
 sudo sysctl -w net.inet.tcp.recvspace=1042560
 # http://coryklein.com/tcp/2015/11/25/custom-configuration-of-tcp-socket-keep-alive-timeouts.html
